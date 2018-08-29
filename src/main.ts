dddafd1659e22bc3c664fab51638fe65f2496c1f
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-08-29 15:45:28 +0900

    Remove jquery

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@
  * cf. https://qiita.com/komakomako/items/8efd4184f6d7cf1363f2#comment-b4821ac62eea8fea0dfa
  * cf. https://codepen.io/anon/pen/wrzgQL
  */
-import $ from 'jquery';
-
 const isSupportedImage = file =>
   file.type === 'image/jpeg' || file.type === 'image/png';
 
@@ -15,16 +13,20 @@ class App {
 
   constructor() {
     const self = this;
-    $('#upload').on('click', function() {
-      return self.handleUpload();
-    });
-    $('input[type=file]').on('change', function() {
-      return self.handleChange(this);
-    });
+    document
+      .querySelector('.upload-button')
+      .addEventListener('click', function() {
+        return self.handleUpload();
+      });
+    document
+      .querySelector('.file-input')
+      .addEventListener('change', function() {
+        return self.handleChange(this);
+      });
   }
 
   private handleChange = element => {
-    const file = $(element).prop('files')[0];
+    const file = element.files[0];
     if (!isSupportedImage(file)) {
       return;
     }
@@ -35,15 +37,12 @@ class App {
   };
 
   private handleLoadImage = (image: HTMLImageElement) => {
-    let width;
-    let height;
     const ratio = image.naturalWidth / image.naturalHeight;
     const canvas = document.createElement('canvas');
     canvas.width = ratio >= 1 ? THUMBNAIL_SIZE : THUMBNAIL_SIZE * ratio;
     canvas.height = ratio < 1 ? THUMBNAIL_SIZE : THUMBNAIL_SIZE / ratio;
 
     const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, width, height);
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
     canvas.toBlob(blob => {
@@ -68,6 +67,6 @@ class App {
   }
 }
 
-$(() => {
+document.addEventListener('DOMContentLoaded', () => {
   new App();
 });
